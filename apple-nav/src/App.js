96fd57dff7bf85acc7e4dd95d4f57aec72a7627f
Your Name <tfikrie@yahoo.com>
2019-06-23 23:20:34 -0400

    MacHome components added

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation';
 import { Route } from 'react-router-dom';
 import AppleHome from './components/appleHome';
 import Mac from './components/Mac';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props){
@@ -15,7 +16,7 @@ class App extends React.Component {
       <div className="App">
         <Navigation />
         <Route path='/' exact render={()=> <AppleHome />} />
-        <Route path='/Mac' exact render={()=> <Mac />} />
+        <Route path='/Mac' exact render={()=> <Router><Mac /></Router>} />
       </div>
     );
   }
--- apple-nav/src/components/Mac.js
+++ apple-nav/src/components/Mac.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import mac1 from '../images/mac1.JPG';
-import mac2 from '../images/mac2.JPG';
 import MacNav from './macNav';
 import { Route} from 'react-router-dom';
 import Macbook from './Macbook';
+import  MacHome from './macHome';
 
 class Mac extends React.Component {
     constructor(props) {
@@ -13,13 +12,7 @@ class Mac extends React.Component {
      return (
          <div>
              <MacNav />
-             <h1>Mac Pro</h1>
-             <h1>Power to change </h1>
-             <h1>everything.</h1>
-             <img src={mac1} alt='mac book' />
-             <h3>Pro Display XDR</h3>
-             <h1>Believing is seeing.</h1>
-             <img src={mac2} alt='mac 3d' />
+             <MacHome />
              <Route path='/macbook' exact render={()=> <Macbook />} />
          </div>
      )
